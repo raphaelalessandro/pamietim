5ab3fc6d51c37d5e4dccfef51f8c5bb500a6f28d
raphaelalessandro <160646952+raphaelalessandro@users.noreply.github.com>
2024-11-01 09:43:31 -0300

    aula 01-11

--- App.js
+++ App.js
@@ -4,12 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
 import Exemplo1 from './scr/exemplos/ex_01';
+import Exemplo2 from './scr/exemplos/ex_02';
+
 import Atividade1 from './scr/atividades/atv_01';
+import Atividade2 from './scr/atividades/atv_02';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade1/>
+      <Exemplo2/>
       <StatusBar style="auto" />
     </View>
   );
